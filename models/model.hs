fe72dfa45adfbbe018ba4860e4c0820b5e456944
romue404 <robert.mueller1990@googlemail.com>
2017-07-29 13:41:52 +0200

    initial commit

---
+++ models/model.hs
@@ -0,0 +1,49 @@
+data Card = Card {value :: Value, color :: Color} deriving (Read, Show, Eq)
+data Color = Eichel | Gras | Herz | Schellen deriving (Read, Show, Enum, Eq, Ord)
+data Value = Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Read,  Show, Enum, Eq, Ord)
+
+type Cards = [Card]
+type PlayerNumber = Integer
+data Player = Player {name :: String, number :: PlayerNumber}
+
+
+colors :: [Color]
+colors = [Eichel .. Schellen]
+
+values :: [Value]
+values = [Six .. Ace]
+
+deck :: Deck
+deck = [Card value color | color <- colors, value <- values]
+
+data PlayerMove = PlayCard Card | TellNumberOfTricks Integer deriving (Read, Show, Eq)
+data GameAction = WaitingForTricks PlayerNumber
+
+data PlayerState = PlayerState {hand :: Cards, tricks :: [Integer], score :: [Integer]}
+data GameState = GameState {
+  deck :: Cards,
+  trump :: Card,
+  playersTurn :: Player,
+  playersState :: Map Player PlayerState }
+
+{-|
+  cardsPerPlayer round:Int  draw a specified ammount of cards from the deck
+  draw amount: Int  draw a specified ammount of cards from the deck
+  draw trump
+  required -> gameState -> Color | Value tells you wich kind of card you need to play
+  isValidMove -> Boolean tells you if your move was correct
+  tellNumberOfTricks -> Player tell the game how many tricks you would like to make, save tricks to game state
+  makeMove
+  evaluateRound
+  setUpNextRound
+  isGameOver
+  makeGameSummary
+  Geber -> cannot estimate #ofTricks so that they match the number of rounds
+  allPossibleCards
+-}
+
+deal :: Cards -> Int -> (Cards, Cards)
+deal deck numberCards = (take numberCards deck, drop numberCards deck)
+
+main = do
+  show (deck `deal` 5 )
