d7aaa665db5c8ce434769177cb8031869f83b66e
zhenhaoli <zhenhao.li@outlook.com>
2017-08-24 16:58:52 +0200

    add ui for game info and game score

--- ui/game.js
+++ ui/game.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+
 var connection = new WebSocket("ws://localhost:8080");
 
 let firstStep = {
