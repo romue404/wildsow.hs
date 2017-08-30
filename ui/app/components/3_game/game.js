73ae52a91e89b7b3f4a825c6947c5f6415733c52
zhenhaoli <zhenhao.li@outlook.com>
2017-08-30 21:36:23 +0200

    add card

--- ui/app/components/3_game/game.js
+++ ui/app/components/3_game/game.js
@@ -5,26 +5,44 @@
     .module('wildsow')
     .controller('GameCtrl', GameCtrl);
 
-  GameCtrl.$inject = ['$scope', '$rootScope', '$state', 'localStorageService', 'GameState'];
+  GameCtrl.$inject = ['$scope', '$state', 'localStorageService', 'GameState'];
 
-  function GameCtrl($scope, $rootScope, $state, localStorageService, GameState) {
+  function GameCtrl($scope, $state, localStorageService, GameState) {
 
-    $rootScope.username = localStorageService.get("username");
-    if(!$rootScope.username) $state.go('login');
+    $scope.username = localStorageService.get("username");
+    if(!$scope.username) $state.go('login');
+    $scope.gameId = localStorageService.get("gameId");
 
     // variables
     $scope.about = "Game Page";
+    $scope.currentGameState = GameState.current.state;
 
     $scope.$on('gameStateUpdated', function(event, currentGameState) {
-      $scope.currentGameState = JSON.stringify(currentGameState, null, 2);
+      updateUi(currentGameState);
+      $scope.$apply();
     });
+
+    function updateUi(currentGameState) {
+      var debug = JSON.stringify(currentGameState, null, 2);
+      //console.log(debug)
+
+      $scope.currentGameState = currentGameState || localStorageService.get("gameState");
+
+      if($scope.currentGameState && $scope.currentGameState.playerState){
+        $scope.player = $scope.currentGameState.playerState.filter(
+          ps => ps.player.playerName === $scope.username
+        )[0];
+        console.log($scope.player);
+      }
+    }
 
     // apis
     $scope.tellTricks = tellTricks;
     $scope.playCard = playCard;
-    
+
     function tellTricks() {
-      var action = GameState.createActionRequest("tellTricks", )
+      var action = GameState.createActionRequest("tellTricks", $scope.gameId, $scope.username);
+      GameState.sendActionRequest(action);
     }
 
     // functions
