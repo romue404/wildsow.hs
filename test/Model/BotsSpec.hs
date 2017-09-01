732b2a8f81897384554d23195ed2bcc1861745b8
Christian Lemke <lemkech@gmail.com>
2017-09-01 13:29:35 +0200

    playableCards2Test is working

--- test/Model/BotsSpec.hs
+++ test/Model/BotsSpec.hs
@@ -1,54 +1,63 @@
-module Model.BotsSpec (main, spec) where
+module Model.BotsSpec where
 
 import Test.Hspec
 import Test.QuickCheck
 
-import System.Random(StdGen, newStdGen, next, mkStdGen)
+import System.Random(StdGen, getStdGen, newStdGen, next, mkStdGen)
 
 import Model.Bots
 import Model.Model
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
-main :: IO ()
-main = do
-    gen <- newStdGen
---    hspec spec
-    hspec $ spec gen
 {-
+main :: IO ()
+main = hspec spec
+-}
+
+main :: IO ()
+main = hspec $ spec
+
+--  hspec spec
+
+
+--spec :: StdGen -> Spec
 spec :: Spec
 spec = do
-    describe "aTest" $ do
-        it "add one" $ do
-            aTest 41 `shouldBe` 42
---    it "is idempotent" $ property $
---      \a -> aTest a === aTest (aTest a)
+    describe "botSpecTest" $ do
+         it "add one" $ do
+            addOneTest `shouldBe` addOneTestResult
+         it "test playableCards2" $ do
+            playableCards2Test `shouldBe` playableCards2TestResult
 
--}
-spec :: StdGen -> Spec
-spec gen= do
-    describe "playableCards2" $ do
-        it "test playableCards2..." $ do
-            playeableCards2 (trump gs) theCurrentColor (hand playerState1) `shouldBe` result
-            where
-            theCurrentColor = Gras
-            theTrump = Gras
-            theHand = [Card{value=Nine, color=Herz}, Card{value=Seven, color=Herz}, Card{value=King, color=Herz}, Card{value=Ace, color=Herz}]
-            playerState1 = PlayerState{
-                player = RandomBot {playerName="TestBotName"},
-                playedCard = Nothing,
-                hand = theHand,
-                tricks = [0],
-                score = [0],
-                tricksSubround =[(0,0)]
-            }
-            gs = GameState{
-                phase = Idle,
-                currentRound = 1,
-                currentColor = Nothing,
-                pile = [],
-                trump = theTrump,
-                playerStates= [playerState1],
-                stdGen=gen
-            }
-            result = [Card{value=Nine, color=Herz}, Card{value=Seven, color=Herz}, Card{value=King, color=Herz}, Card{value=Ace, color=Herz}]
+--testtest
+addOneTest = aTest 41
+addOneTestResult = 42
+
+--
+playableCards2Test :: Cards
+playableCards2Test = playeableCards2 (trump gs) theCurrentColor (hand playerState1)
+    where
+    theCurrentColor = Herz
+    theTrump = Gras
+    theHand = [Card{value=Nine, color=Gras}, Card{value=Seven, color=Schellen}, Card{value=King, color=Gras}, Card{value=Ace, color=Herz}]
+    playerState1 = PlayerState{
+        player = RandomBot {playerName="TestBotName"},
+        playedCard = Nothing,
+        hand = theHand,
+        tricks = [0],
+        score = [0],
+        tricksSubround =[(0,0)]
+    }
+    gs = GameState{
+        phase = Idle,
+        currentRound = 1,
+        currentColor = Nothing,
+        pile = [],
+        trump = theTrump,
+        playerStates= [playerState1],
+        stdGen = mkStdGen 42
+    }
+
+playableCards2TestResult :: Cards
+playableCards2TestResult = [Card{value=Ace, color=Herz}]
